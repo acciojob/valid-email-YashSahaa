a875f942a62218aec70de0c96514c193ef986322
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-24 21:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.length==0) return false
+	else if(!str.includes("@")) return false
+	else{
+		let arr = str.split("@")
+		if(arr.length>2) return false
+		else if(arr[0].includes(".")) return false
+	}
+	return true
 }
 
 // Do not change the code below.
